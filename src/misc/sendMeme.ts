b882caf5f4cd47e022632869bc04c2d6a488246d
James Yeoman <jamesyeoman10@gmail.com>
2025-09-04 09:44:41 +0100

    test

--- src/misc/sendMeme.ts
+++ src/misc/sendMeme.ts
@@ -1,13 +1,13 @@
 import type { RequestHandler } from '../types.js';
 
-import { getHentaiThread } from '../utils.js';
+import { getHentaiThread, getTestChannel } from '../utils.js';
 
-type Payload = { caption?: string; media: string };
+type Payload = { caption?: string; media?: string };
 export const sendMeme = (payload: Payload): RequestHandler => {
   return async (client) => {
-    const ht = await getHentaiThread(client);
+    const ht = await getTestChannel(client);
 
     if (payload.caption) await ht.send(payload.caption);
-    await ht.send(payload.media);
+    if (payload.media) await ht.send(payload.media);
   };
 };
