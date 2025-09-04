b87134adc5c8ac42dcb7ff6769a46a0feb191713
James Yeoman <jamesyeoman10@gmail.com>
2025-09-04 09:52:29 +0100

    silksong :)

--- src/misc/sendMeme.ts
+++ src/misc/sendMeme.ts
@@ -1,11 +1,11 @@
 import type { RequestHandler } from '../types.js';
 
-import { getHentaiThread, getTestChannel } from '../utils.js';
+import { getHentaiThread } from '../utils.js';
 
 type Payload = { caption?: string; media?: string };
 export const sendMeme = (payload: Payload): RequestHandler => {
   return async (client) => {
-    const ht = await getTestChannel(client);
+    const ht = await getHentaiThread(client);
 
     if (payload.caption) await ht.send(payload.caption);
     if (payload.media) await ht.send(payload.media);
