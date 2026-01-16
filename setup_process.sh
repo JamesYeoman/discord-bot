ecfb95d8bbaa78170ad11561a7aa4a9f34d9c8c3
James Yeoman <jamesyeoman10@gmail.com>
2026-01-16 16:35:36 +0000

    nodenv -> asdf

--- setup_process.sh
+++ setup_process.sh
@@ -1,49 +1,24 @@
 #!/usr/bin/env bash
 
-if ! command -v nodenv &> /dev/null; then
-    if [[ -d "${HOME}/.nodenv" ]]; then
-        echo "Nodenv exists on system, but not setup in shell!"
-        echo "Will do a temporary workaround, but it's recommended to properly"
-        echo "setup nodenv!"
-    else
-        echo "Nodenv not installed! Installing Nodenv!"
-        git clone https://github.com/nodenv/nodenv.git "${HOME}/.nodenv"
-        pushd "${HOME}/.nodenv" && src/configure && make -C src
-        popd
-        echo 'export PATH="$HOME/.nodenv/bin:$PATH"' >> ~/.bashrc
-        mkdir -p "$(nodenv root)"/plugins
-        git clone https://github.com/nodenv/node-build.git "$(nodenv root)"/plugins/node-build
-    fi
-
-
-    if [[ "${NODENV_HOME:-unset}" == "unset" ]]; then
-        export NODENV_HOME="${HOME}/.nodenv"
-    fi
-
-    export PATH="${NODENV_HOME}/bin:$PATH"
-
-    eval "$(nodenv init -)"
-fi
-
-if nodenv which node | grep 'is not installed' &>/dev/null; then
+if asdf which node | grep 'unknown command' &>/dev/null; then
     echo "The repo's node version is not installed!"
     echo "Installing..."
-    nodenv install
+    asdf install
 fi
 
-if ! nodenv which yarn &> /dev/null; then
+if ! asdf which yarn &> /dev/null; then
     echo "Yarn not installed! Installing Yarn!"
     npm install -g yarn
-    nodenv rehash
+    asdf reshim
 
     echo "You should run `yarn install` now, before starting this script again!"
     exit 1
 fi
 
-if ! nodenv which pm2 &> /dev/null; then
+if ! asdf which pm2 &> /dev/null; then
     echo "Installing PM2"
     npm install -g pm2
-    nodenv rehash
+    asdf reshim
 fi
 
 echo "Registering PM2 services"
